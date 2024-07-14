ca5e7e728d86776a4634065407d7dfb3f034f4e2
krjofficial <krishrjain.official@gmail.com>
2024-07-14 08:11:54 +0530

    Created and Registered Models

--- DocRecApp/admin.py
+++ DocRecApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import Location, Doctor, Patient, Visit
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Visit)
+
