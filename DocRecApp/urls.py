7aae557f34e3abdeeefd2d95773e88ba7919755b
krjofficial <krishrjain.official@gmail.com>
2024-07-18 22:49:26 +0530

    frontend functionality

--- DocRecApp/urls.py
+++ DocRecApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import base 
+from . import views
 
 urlpatterns = [
-    path('', base, name="base")
+    path('', base, name="base"),
+    path('doctor_details/<int:pk>/', views.doctor_details, name="doctor_details" ),
+    path('book_appointment/<int:pk>/', views.book_appointment, name='book_appointment'),
 ]
--- DocRecApp/views.py
+++ DocRecApp/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Doctor, Patient
 
 # Create your views here.
 
 def base(request):
-    return render(request, 'base.html')
+
+    doctors = Doctor.objects.all()
+    patients = Patient.objects.all()
+    return render(request, 'base.html', {'doctors':doctors, 'patients': patients})
+
+
+def doctor_details(request, pk):
+    doctor = get_object_or_404(Doctor, id=pk)
+    
+    return render(request, 'doctor_details.html', {'doctor': doctor})
+    
+
+def book_appointment(request, pk):
+    doctor = Doctor.objects.get(id=pk)
+    doctor.visit_count += 1
+    doctor.save()
+    return redirect('base')
+
+
+
+# doctor name - clickable redirect doctor details page
+# doctor details - name, id, longitude, latitude, visit count, button - book appointment
+# book appointment - increment the visit count by 1 and redirect home
